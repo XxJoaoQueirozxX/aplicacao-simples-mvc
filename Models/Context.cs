6bf04d020fe9bd5077ab7cf80eb799ed995e130f
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-02-04 00:15:36 -0300

    commit inicial

---
+++ Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mvc.Models;
+
+namespace mvc.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=mvc;Integrated Security=True");
+        }
+        public DbSet<mvc.Models.Produto> Produto { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Aplicação ASP.NET MVC + EntityFramework de Produtos e Categorias 
